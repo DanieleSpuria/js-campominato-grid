ce0a603bba867a1c28c85122929238bfe1b33cfb
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-03-29 15:46:14 +0200

    fix .container, add function cell

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,28 @@
 3. inserire tutto nel click di un bottone;
 4. inserire toggle a click delle celle;
 *************************************************************************************/
+
+
+
+const container = document.getElementsByClassName('container');
+cell(100);
+
+
+
+
+
+
+//*********************************************************************** Funzioni ***
+
+function cell(num) {
+
+  for (let i = 0; i < num; i++) {
+
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+    container[0].appendChild(cell);
+
+  }
+
+}
+
