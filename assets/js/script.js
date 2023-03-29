9cae27d124d4e26e4cdbf734acb2b4f10499beae
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-03-29 15:24:48 +0200

    add main, .container, .cell

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/*************************************************************************************
+1. classi con css;
+2. griglia con for;
+3. numerare le celle;
+3. inserire tutto nel click di un bottone;
+4. inserire toggle a click delle celle;
+*************************************************************************************/
