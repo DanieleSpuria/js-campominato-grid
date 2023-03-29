0311fa09a85671a3f9cb50fbb2049e6d827e08f1
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-03-29 20:04:54 +0200

    rmv .containerGriglia

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,6 @@
 
 //*************************************************************************** Html ***
 const container = document.getElementsByClassName('container')[0];
-const containerGriglia = document.getElementsByClassName('container-griglia')[0];
 const griglia = document.getElementsByClassName('griglia')[0];
 const btnReturn = document.getElementById('return');
       btnReturn.classList.add('d-none');
@@ -23,13 +22,13 @@ const btn49 = document.getElementById('49');
 
 
 //************************************************************************ Bottoni ***
-btnClick(btn100, btnReturn, btn100, btn81, btn49, 100, 'calc(100% / 10)', containerGriglia, griglia, '100%', '100%');
-btnClick(btn81, btnReturn, btn100, btn81, btn49, 81, 'calc(100% / 9)', containerGriglia, griglia, '90%', '90%');
-btnClick(btn49, btnReturn, btn100, btn81, btn49, 49, 'calc(100% / 7)', containerGriglia, griglia, '70%', '70%');
+btnClick(btn100, btnReturn, btn100, btn81, btn49, 100, 'calc(100% / 10)', griglia, '100%');
+btnClick(btn81, btnReturn, btn100, btn81, btn49, 81, 'calc(100% / 9)', griglia, '90%');
+btnClick(btn49, btnReturn, btn100, btn81, btn49, 49, 'calc(100% / 7)', griglia, '70%');
 
 btnReturn.addEventListener('click', function() {
     
-  containerGriglia.classList.remove('d-flex');
+  griglia.classList.remove('d-flex');
   btnReturn.classList.add('d-none');
   btn100.classList.remove('d-none');
   btn81.classList.remove('d-none');
@@ -40,7 +39,7 @@ btnReturn.addEventListener('click', function() {
 
 
 //*********************************************************************** Funzioni ***
-function cells(num, width, box2) {
+function cells(num, width, box) {
   
   for (let i = 0; i < num; i++) {
     
@@ -48,7 +47,7 @@ function cells(num, width, box2) {
     cell.className = 'cell';
     cell.style.width = width;
     cell.id = i + 1;
-    box2.appendChild(cell);
+    box.appendChild(cell);
     
     cell.addEventListener('click', function() {
       
@@ -59,15 +58,14 @@ function cells(num, width, box2) {
   } 
 }
 
-function btnClick(btn, btn1, btn2, btn3, btn4, num, width, box, box2, widthBox2, heigthBox2) {
+function btnClick(btn, btn1, btn2, btn3, btn4, num, width, box, widthBox) {
   
   btn.addEventListener('click', function() {
 
-    box2.innerHTML = '';
-    box2.style.width = widthBox2;
-    box2.style.heigth = heigthBox2;
-    cells(num, width, box2);
+    box.innerHTML = '';
+    box.style.width = widthBox;
     box.classList.add('d-flex');
+    cells(num, width, box);
     btn1.classList.remove('d-none');
     btn2.classList.add('d-none');
     btn3.classList.add('d-none');
